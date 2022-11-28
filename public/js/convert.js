468ca5a8447a81b33406c9ee8002294c18a0f99b
Logan <thecrazykid2121@gmail.com>
2022-11-28 16:59:39 -0500

    push

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,4 +1,5 @@
 let input = document.querySelector('input')
+let gameType = localStorage.getItem("gameType");
 let gameData = "";
 input.addEventListener('change', () => {
 let file = input.files[0];
@@ -6,6 +7,7 @@ async function convertFile() {
 let test = JSON.stringify(Array.from(new Uint8Array(await (new Blob([file])).arrayBuffer())));
 console.log(test);
 console.log(file);
+console.log(gameType);
 gameData = test;
 }
 convertFile();
--- public/js/libget.js
+++ public/js/libget.js
@@ -1,6 +1,30 @@
 var localGames = "";
 var localGamesArray = [];
 let gameType = "";
+let emulatorCheck = true;
+let emulatorState = "";
+
+if (emulatorCheck === null) {
+    emulatorState = "../emulators/false.html";
+    console.log("Set emulator check to false")
+} else if (emulatorCheck === true) {
+    emulatorState = "../emulators/emulatorjs.html";
+    console.log("Emulator check is true")
+} else if (emulatorCheck === false) {
+    emulatorState = "../emulators/false.html";
+    console.log("Emulator check is false")
+}
+
+if (localStorage.getItem("localGames") === null) {
+    console.log("No local games found, creating new array");
+  }
+    else {
+        localGames = localStorage.getItem("localGames");
+        localGamesArray = JSON.parse(localGames);
+        document.getElementById("libContainer").innerHTML = localGamesArray;
+        console.log("Local games found, loading array");
+        modalWait();
+    }
 let i = document.querySelector('input').addEventListener('change', (e)=>{
     const files = e.target.files;
   const fileNames = [...files].map((file) => file.name);
@@ -9,12 +33,13 @@ let i = document.querySelector('input').addEventListener('change', (e)=>{
   var str = '<ul>'
 
 fileNames.forEach(function(slide) {
-  str += '<li id="' + fileNames.indexOf(slide) +'">'+ slide + '</li>';
+  str += '<a onclick="" class="libCont" href="' + emulatorState + '" id="' + fileNames.indexOf(slide) +'">'+ slide + '</a> <br>';
 }); 
 
 str += '</ul>';
 document.getElementById("libContainer").innerHTML = str;
-getId();
+localStorage.setItem("localGames", JSON.stringify(document.getElementById("libContainer").innerHTML));
+modalWait();
   })
 
 
@@ -27,13 +52,26 @@ function getId() {
     }, false);
     
 }
+getId();
   
 function getLib(id) {
     let gameName = document.getElementById(id);
 
     if (gameName.textContent.includes('.gba')) {
         gameType = "gba";
+        localStorage.setItem("gameType", gameType);
+        console.log("Set game type")
     }
+    if (gameName.textContent.includes('.nds')) {
+        gameType = "nds";
+        localStorage.setItem("gameType", gameType);
+    }
+    if (gameName.textContent.includes('.nes')) {
+        gameType = "nes";
+        localStorage.setItem("gameType", gameType);
+        console.log("Set game type")
+    }
+    
 }
 
 function triggerHome() {
@@ -49,4 +87,32 @@ function triggerDisc() {
 
 function triggerSave() {
     window.top.location.href = "library.html";
+}
+
+function modalWait() {
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 }
