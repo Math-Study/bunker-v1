4a67d5293b73cd23044d44661505918b02a65876
Logan <thecrazykid2121@gmail.com>
2022-11-28 07:21:07 -0500

    push

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,11 +1,12 @@
 let input = document.querySelector('input')
-
+let gameData = "";
 input.addEventListener('change', () => {
-let file = input.files;
+let file = input.files[0];
 async function convertFile() {
 let test = JSON.stringify(Array.from(new Uint8Array(await (new Blob([file])).arrayBuffer())));
 console.log(test);
 console.log(file);
+gameData = test;
 }
 convertFile();
 });
