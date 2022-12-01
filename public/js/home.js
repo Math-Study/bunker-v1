7345551118873056226e68da85ce3399faf7b1f9
Logan <thecrazykid2121@gmail.com>
2022-12-01 10:29:13 -0500

    Changelog Added

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+function triggerHome() {
+    window.top.location.href = "../../index.html"; 
+}
+function triggerSet() {
+    window.top.location.href = "../settings.html";
+}
+
+function triggerDisc() {
+    window.top.location.href = "https://discord.gg/cT7cApxHJ2";
+}
+
+function triggerSave() {
+    window.top.location.href = "library.html";
+}
