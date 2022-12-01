8bfe21090b89adfce105cc997536e4199789fc79
Logan <thecrazykid2121@gmail.com>
2022-12-01 10:19:56 -0500

    Settings Page!

---
+++ public/js/settings.js
@@ -0,0 +1,35 @@
+let input = document.getElementById("startup");
+let startupVal = document.getElementById("startupVal");
+let inputDat = localStorage.getItem("startup");
+
+
+if (inputDat == null) {
+    console.log("No startup value found");
+} else {
+    console.log("Startup value found, loading");
+    input.value = inputDat;
+    startupVal.innerHTML = "Startup Site set!";
+}
+input.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        console.log(input.value);
+       let startupValue = input.value;
+    localStorage.setItem("startup", startupValue);
+    startupVal.innerHTML = "Startup Site set!";
+        }
+});
+
+function triggerHome() {
+    window.top.location.href = "../index.html"; 
+}
+function triggerSet() {
+    window.top.location.href = "settings.html";
+}
+
+function triggerDisc() {
+    window.top.location.href = "https://discord.gg/cT7cApxHJ2";
+}
+
+function triggerSave() {
+    window.top.location.href = "library.html";
+}
--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,6 +1,14 @@
 let viewer = document.getElementById("viewer");
 let searchBar = document.getElementById("searchBar");
+let startup = localStorage.getItem("startup");
 
+if (startup == null) {
+    console.log("No startup value found");
+} else {
+    console.log("Startup value found, loading");
+    viewer.src = startup;
+    viewer.style.display = "block";
+}
 
 //event listener for enter key, then replace viewer sort with search results
 searchBar.addEventListener('keypress', function (e) {
