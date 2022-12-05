4029ca634ce3df60b4e5ba7a98b83cbced7f7a0d
Logan <thecrazykid2121@gmail.com>
2022-12-05 11:00:24 -0500

    push

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,3 +1,16 @@
-//BunkerConverter GOES HERE @i10
+window.globalGameIntercept = {};
+        globalGameIntercept.register = function(name, content){
+            let coder = new TextEncoder();
+            let bin = coder.encode(decodeURIComponent(escape(window.atob(content))));
+            window.gameData = bin;
+            EJS_player = '#game';
+            EJS_core = "gba";
+            EJS_color = "#de4587";
+            EJS_gameUrl = URL.createObjectURL(new Blob([Uint8Array.from(window.gameData)]));
+            EJS_pathtodata = "https://rawcdn.githack.com/EmulatorJS/EmulatorJS/main/data/";
+            
+        }
 
-// Is linked to lib/index.html (library) and emulators/emulatorjs (you can remove from emulator)
+
+
+       
