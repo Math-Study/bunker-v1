9007ff3624271869d92c11af525111156d3d651b
Logan <thecrazykid2121@gmail.com>
2022-12-06 16:18:34 -0500

    small push for decoder

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,14 +1,18 @@
 window.globalGameIntercept = {};
         globalGameIntercept.register = function(name, content){
             let coder = new TextEncoder();
-            let bin = coder.encode(decodeURIComponent(escape(window.atob(content))));
+           // let bin = coder.encode(decodeURIComponent(encodeURIComponent(window.atob(content))));
+            let decoded = window.atob(content);
+            let bin = coder.encode(decoded);
             window.gameData = bin;
             EJS_player = '#game';
             EJS_core = "gba";
+            EJS_DEBUG_XX = true;
+            EJS_gameName = name;
             EJS_color = "#de4587";
-            EJS_gameUrl = URL.createObjectURL(new Blob([Uint8Array.from(window.gameData)]));
             EJS_pathtodata = "https://rawcdn.githack.com/EmulatorJS/EmulatorJS/main/data/";
-            
+            EJS_gameUrl = URL.createObjectURL(new Blob([Uint8Array.from(window.gameData)]));
+            console.log(bin);
         }
 
 
