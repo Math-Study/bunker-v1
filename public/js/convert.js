f00a03bf10a24213a789fa917b33df6f9ae5ff73
Logan <thecrazykid2121@gmail.com>
2022-12-07 08:15:25 -0500

    small push

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,14 +1,13 @@
 window.globalGameIntercept = {};
         globalGameIntercept.register = function(name, content){
             let coder = new TextEncoder();
-           // let bin = coder.encode(decodeURIComponent(encodeURIComponent(window.atob(content))));
-            let decoded = window.atob(content);
-            let bin = coder.encode(decoded);
+            bar = coder.encode(decodeURIComponent(escape(window.atob(content))));
+            let bin = new Uint8Array(bar);
             window.gameData = bin;
             EJS_player = '#game';
             EJS_core = "gba";
             EJS_DEBUG_XX = true;
-            EJS_gameName = name;
+            EJS_gameName = 'PokemonSaph';
             EJS_color = "#de4587";
             EJS_pathtodata = "https://rawcdn.githack.com/EmulatorJS/EmulatorJS/main/data/";
             EJS_gameUrl = URL.createObjectURL(new Blob([Uint8Array.from(window.gameData)]));
