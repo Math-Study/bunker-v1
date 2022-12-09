0306994064631502fc9d5efb7c08d4533ab86497
Logan <thecrazykid2121@gmail.com>
2022-12-09 08:18:46 -0500

    ima push this if someone else wants to ask AI to fix it
    Co-Authored-By: Nailington <111466491+Nailington@users.noreply.github.com>

--- public/js/convert.js
+++ public/js/convert.js
@@ -1,21 +1,26 @@
 window.globalGameIntercept = {};
         globalGameIntercept.register = function(name, content){
-            let coder = new TextEncoder();
-            bar = coder.encode(decodeURIComponent(escape(window.atob(content))));
-            console.log(bar);
-            let bin = new Uint8Array(bar);
-            //let bin = JSON.stringify(bol); // JSON.stringify(Array.from(new Uint8Array(await (new Blob([file])).arrayBuffer()))); this is what I am given by ethan
-            window.gameData = bin;
-            EJS_player = '#game';
-            EJS_core = "gba";
-            EJS_DEBUG_XX = true;
-            EJS_gameName = 'advancewars';
-            EJS_color = "#de4587";
-            EJS_pathtodata = "https://rawcdn.githack.com/EmulatorJS/EmulatorJS/main/data/";
-            EJS_gameUrl = URL.createObjectURL(new Blob([Uint8Array.from(window.gameData)]));
-            console.log(bin);
-            /* var fileName = "myfile.json";
-            var fileContent = bin;
+        convert(content);         
+  }
+
+       
+  async function convert(content) {
+        let bin = JSON.stringify(Array.from(new Uint8Array(await (await fetch('data:text/plain;base64,'+content)).arrayBuffer())));
+        window.gameData = bin;
+        EJS_player = '#game';
+        EJS_core = "gba";
+        EJS_DEBUG_XX = true;
+        EJS_gameName = 'advancewars';
+        EJS_color = "#de4587";
+        EJS_pathtodata = "https://rawcdn.githack.com/EmulatorJS/EmulatorJS/main/data/";
+        EJS_gameUrl = URL.createObjectURL(new Blob([Uint8Array.from(window.gameData)]));
+        console.log(bin);
+        
+        
+        
+        //let bin = JSON.stringify(bol); // JSON.stringify(Array.from(new Uint8Array(await (new Blob([file])).arrayBuffer()))); this is what I am given by ethan
+        /* var fileName = "myfile.json";
+        var fileContent = bin;
 var myFile = new Blob([fileContent], {type: 'json/json'});
 
 window.URL = window.URL || window.webkitURL;
@@ -23,8 +28,5 @@ var dlBtn = document.getElementById("download");
 
 dlBtn.setAttribute("href", window.URL.createObjectURL(myFile));
 dlBtn.setAttribute("download", fileName);*/
-        }
-
-
-
-       
+    }
+        
