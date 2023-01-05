5b37c9ec7cef0d3820992ab9dbe684a21d8749f2
Logan <69491394+Cattn@users.noreply.github.com>
2023-01-05 00:11:05 +0000

    CSS Changes, Calculator added, Other pages cleaned up, other things added, settings changed, themes added, overall generally big push
    Worked on by: Nailington, Cattn, ProudParrot2, Latebat, JustJ_rdan

---
+++ DevsPlsRead.md
@@ -0,0 +1,6 @@
+<h1>Pls wait...</h1>
+
+
+All the files are in /public btw devs!
+/public/index.html, /public/folders ETC,
+k, i'm just 
--- public/js/theme.js
+++ public/js/theme.js
@@ -4,50 +4,55 @@ let userSrc = "https://docutyper.com/doc?topic=Cat";
 
 //set themes if they change it
 function themeValue(toggleState) {
-    if (toggleState == 1) {
-        document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light');
-    }
-    if (toggleState == 2) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-      
-      localStorage.setItem('theme', 'dark');
-    }
-    if (toggleState == 3) {
-        document.documentElement.setAttribute('data-theme', 'pink');
-      
-      localStorage.setItem('theme', 'pink');
-    }
-    if (toggleState == 4) {
-      document.documentElement.setAttribute('data-theme', 'green');
+  if (toggleState == 1) {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+  if (toggleState == 2) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+
+    localStorage.setItem('theme', 'dark');
+  }
+  if (toggleState == 3) {
+    document.documentElement.setAttribute('data-theme', 'pink');
+
+    localStorage.setItem('theme', 'pink');
+  }
+  if (toggleState == 4) {
+    document.documentElement.setAttribute('data-theme', 'green');
 
     localStorage.setItem('theme', 'green');
   }
   if (toggleState == 5) {
     document.documentElement.setAttribute('data-theme', 'dark-green');
 
-  localStorage.setItem('theme', 'dark-green');
-}
-if (toggleState == 6) {
-  document.documentElement.setAttribute('data-theme', 'black-white');
+    localStorage.setItem('theme', 'dark-green');
+  }
+  if (toggleState == 6) {
+    document.documentElement.setAttribute('data-theme', 'black-white');
 
-localStorage.setItem('theme', 'black-white');
-}
-if (toggleState == 7) {
-  document.documentElement.setAttribute('data-theme', 'blue-pink');
+    localStorage.setItem('theme', 'black-white');
+  }
+  if (toggleState == 7) {
+    document.documentElement.setAttribute('data-theme', 'blue-pink');
 
-localStorage.setItem('theme', 'blue-pink');
-}
-if (toggleState == 8) {
-  document.documentElement.setAttribute('data-theme', 'charcoal-yellow');
+    localStorage.setItem('theme', 'blue-pink');
+  }
+  if (toggleState == 8) {
+    document.documentElement.setAttribute('data-theme', 'charcoal-yellow');
 
-localStorage.setItem('theme', 'charcoal-yellow');
-}
-if (toggleState == 9) {
-  document.documentElement.setAttribute('data-theme', 'hotpink-cyan');
+    localStorage.setItem('theme', 'charcoal-yellow');
+  }
+  if (toggleState == 9) {
+    document.documentElement.setAttribute('data-theme', 'hotpink-cyan');
 
-localStorage.setItem('theme', 'hotpink-cyan');
-}
+    localStorage.setItem('theme', 'hotpink-cyan');
+  }
+  if (toggleState == 10) {
+    document.documentElement.setAttribute('data-theme', 'discord');
+
+    localStorage.setItem('theme', 'discord');
+  }
 }
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
@@ -72,29 +77,29 @@ function panicPage(srcId, topic) {
 }
 
 document.onkeyup = function(e) {
-if (e.ctrlKey && e.shiftKey && e.which == 72) {
-  console.log("key is 0")
-  if (key == 2) {
-    document.body.removeChild(document.body.lastChild);
-    key = 0;
-  }else {
-    key = 1;
-    console.log("key is 1");
+  if (e.ctrlKey && e.shiftKey && e.which == 72) {
+    console.log("key is 0")
+    if (key == 2) {
+      document.body.removeChild(document.body.lastChild);
+      key = 0;
+    } else {
+      key = 1;
+      console.log("key is 1");
+    }
+    if (key == 1) {
+      console.log("key is 1");
+      var ifrm = document.createElement("iframe");
+      ifrm.setAttribute("src", userSrc);
+      ifrm.style.width = "100%";
+      ifrm.style.height = "100%";
+      ifrm.style.zIndex = "999999";
+      ifrm.style.position = "fixed";
+      ifrm.style.top = "0";
+      ifrm.style.left = "0";
+      ifrm.style.border = "none";
+      document.body.appendChild(ifrm);
+      key = 2;
+    }
   }
-  if (key == 1) {
-    console.log("key is 1");
-    var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", userSrc);
-  ifrm.style.width = "100%";
-  ifrm.style.height = "100%";
-  ifrm.style.zIndex = "999999";
-  ifrm.style.position = "fixed";
-  ifrm.style.top = "0";
-  ifrm.style.left = "0";
-  ifrm.style.border = "none";
-  document.body.appendChild(ifrm);
-  key = 2;
-  } 
-}
 };
 
